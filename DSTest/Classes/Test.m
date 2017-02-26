59018e03f63990c423b1a3c569440cd5aa873a5c
zhangdasen <zhangdasen@126.com>
2017-02-27 00:06:51 +0800

    m

---
+++ DSTest/Classes/Test.m
@@ -0,0 +1,17 @@
+
+//
+//  Test.m
+//  Test项目
+//
+//  Created by 张大森 on 2017/2/26.
+//  Copyright © 2017年 张大森. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
++ (void)flog
+{
+    NSLog(@"logo logo");
+}
+@end
